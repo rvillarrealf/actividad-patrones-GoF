5993ec6923ed535712f429439a74e61a93784027
Pipe1844 <pipevifa@gmail.com>
2026-06-08 19:05:47 -0600

    paso-1: implemento metodo fabrica

--- src/main/kotlin/patterns/creational/FactoryMethod.kt
+++ src/main/kotlin/patterns/creational/FactoryMethod.kt
@@ -1,28 +1,57 @@
-package patterns.creational
+//package patterns.creational
+//
+//interface ProductoPendienteFactoryMethod {
+//    fun descripcion(): String
+//}
+//
+//abstract class TallerPendiente {
+//    fun prepararPedido(): String {
+//        val producto = crearProductoPendiente()
+//        return "Pedido preparado para ${producto.descripcion()}"
+//    }
+//
+//    protected open fun crearProductoPendiente(): ProductoPendienteFactoryMethod {
+//        // TODO: reemplaza este método por un verdadero método fábrica abstracto.
+//        return object : ProductoPendienteFactoryMethod {
+//            override fun descripcion(): String = "producto temporal"
+//        }
+//    }
+//}
+//
+//class FactoryMethodDemo {
+//    fun ejecutar(): String {
+//        // TODO: crea un creador concreto y úsalo desde aquí.
+//        return TallerLocal().prepararPedido()
+//    }
+//}
+//
+//class TallerLocal : TallerPendiente()
 
-interface ProductoPendienteFactoryMethod {
+interface Producto {
     fun descripcion(): String
 }
 
-abstract class TallerPendiente {
+class ProductoDigital : Producto {
+    override fun descripcion(): String = "Producto digital (descarga inmediata)"
+}
+
+abstract class Taller {
+
     fun prepararPedido(): String {
-        val producto = crearProductoPendiente()
-        return "Pedido preparado para ${producto.descripcion()}"
+        val producto = crearProducto()
+        return "Pedido preparado para: ${producto.descripcion()}"
     }
 
-    protected open fun crearProductoPendiente(): ProductoPendienteFactoryMethod {
-        // TODO: reemplaza este método por un verdadero método fábrica abstracto.
-        return object : ProductoPendienteFactoryMethod {
-            override fun descripcion(): String = "producto temporal"
-        }
-    }
+    protected abstract fun crearProducto(): Producto
+}
+
+class TallerLocal : Taller() {
+    override fun crearProducto(): Producto = ProductoDigital()
 }
 
 class FactoryMethodDemo {
     fun ejecutar(): String {
-        // TODO: crea un creador concreto y úsalo desde aquí.
-        return TallerLocal().prepararPedido()
+        val taller: Taller = TallerLocal()
+        return taller.prepararPedido()
     }
 }
-
-class TallerLocal : TallerPendiente()
